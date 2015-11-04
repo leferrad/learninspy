355d6d88a12079b0f9f60a9a096b0696cf9aee3f
Leandro Ferrado <ljferrado@gmail.com>
2015-11-04 00:51:33 -0300

    Agrego en PCA una funcion para calcular un k 'optimo' por defecto

--- utils/feature.py
+++ utils/feature.py
@@ -15,8 +15,9 @@ class PCA(object):
             x = x.features.collect()
         x = np.array(x)
         self.mean = np.mean(x, axis=0)
-        self.std = np.std(x, axis=0)
+        self.std = np.std(x, axis=0, ddof=1)
         self.whitening_offset = 1e-5
+        self.k = None
 
         # Se computa la matriz de covarianza
         cov = np.dot(x.T, x) / x.shape[0]
@@ -29,10 +30,11 @@ class PCA(object):
         self.u = u
         self.v = v
         self.s = s
+        self.k = self._optimal_k()  # Defino una k optima por defecto
 
-    def transform(self, k=None, data=None, standarize=True, whitening=True):
-        if k is None:
-            k = int(self.x.shape[1] * .85)  # retengo 85% de la varianza
+    def transform(self, k=None, data=None, standarize=False, whitening=True):
+        if k is not None:
+            self.k = k
         if data is None:
             data = self.x
         lp_data = False  # Flag que indica que data es LabeledPoint, y debo preservar sus labels
@@ -44,13 +46,23 @@ class PCA(object):
         data -= self.mean  # zero-center sobre data (importante)
         if standarize is True:
             data /= self.std
-        xrot = np.dot(data, self.u[:, :k])
+        xrot = np.dot(data, self.u[:, :self.k])
         if whitening is True:
-            xrot = xrot / np.sqrt(self.s[:k] + self.whitening_offset)
+            xrot = xrot / np.sqrt(self.s[:self.k] + self.whitening_offset)
         if lp_data is True:
             xrot = LabeledDataSet(zip(label, xrot.tolist()))
         return xrot
 
+    def _optimal_k(self, threshold=0.85):
+        # Barrido de k hasta cubrir un threshold de varianza (e.g 85%)
+        var_total = sum(self.s)
+        k_opt = 1
+        for k in xrange(1, len(self.s)):
+            var_explained = sum(self.s[:k]) / var_total
+            if var_explained >= threshold:
+                k_opt = k
+                break
+        return k_opt
 
 
 
