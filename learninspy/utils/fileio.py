4762ce03e5802df66ac4152eec3efc8690617a71
Leandro Ferrado <ljferrado@gmail.com>
2016-01-07 17:44:54 -0300

    Agrego utilidad para configurar logger

--- learninspy/utils/fileio.py
+++ learninspy/utils/fileio.py
@@ -11,6 +11,7 @@ from learninspy.context import sc
 
 # Librerias de Python
 import re
+import logging
 
 text_extensions = ['.dat', '.txt', '.csv']
 
@@ -64,3 +65,23 @@ def save_file(data, path):
     else:
         data.saveAsPickleFile(path)
     return
+
+
+def get_logger(name='Learninspy', level=logging.INFO):
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    return logger
