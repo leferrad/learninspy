061df42d59d33caad09fea65fffd1742122a9c24
Leandro Ferrado <ljferrado@gmail.com>
2016-04-02 16:36:26 -0300

    Actualizo activaciones

--- learninspy/core/activations.py
+++ learninspy/core/activations.py
@@ -4,166 +4,166 @@ __author__ = 'leferrad'
 import numpy as np
 
 
-def tanh(x):
+def tanh(z):
     r"""
     Tangente Hiperbolica
 
-    :math:`f(x)=\dfrac{e^x - e^{-x}}{e^x + e^{-x}}`
+    :math:`f(z)=\dfrac{e^z - e^{-z}}{e^z + e^{-z}}`
 
     """
-    return np.tanh(x)
+    return np.tanh(z)
 
 
-def tanh_d(x):
+def tanh_d(z):
     r"""
     Derivada de Tangente Hiperbolica
 
-    :math:`f(x)=1-tanh^2(x)`
+    :math:`f(z)=1-tanh^2(z)`
 
     """
-    return 1.0 - np.tanh(x) ** 2
+    return 1.0 - np.tanh(z) ** 2
 
 
-def sigmoid(x):
+def sigmoid(z):
     r"""
     Sigmoidea
 
-    :math:`f(x)=\dfrac{1}{1 + e^{-x}}`
+    :math:`f(z)=\dfrac{1}{1 + e^{-z}}`
 
     """
-    return 1.0 / (1.0 + np.exp(-x))
+    return 1.0 / (1.0 + np.exp(-z))
 
 
-def sigmoid_d(x):
+def sigmoid_d(z):
     r"""
     Derivada de Sigmoidea
 
-    :math:`f(x)=\dfrac{e^x - e^{-x}}{e^x + e^{-x}}`
+    :math:`f(z)=\dfrac{e^z - e^{-z}}{e^z + e^{-z}}`
 
     """
-    return sigmoid(x) * (1.0 - sigmoid(x))
+    return sigmoid(z) * (1.0 - sigmoid(z))
 
 
-def relu(x):
+def relu(z):
     r"""
     Rectifier Linear Unit (ReLU)
 
-    :math:`f(x)=max(0,x)`
+    :math:`f(z)=max(0,z)`
 
     """
-    if isinstance(x, np.ndarray) or type(x) == list:
-        x = x[0]
-    return max(0.0, x)
+    if isinstance(z, np.ndarray) or type(z) == list:
+        z = z[0]
+    return max(0.0, z)
 
 
-def relu_d(x):
+def relu_d(z):
     r"""
     Derivada de ReLU
 
-    :math:`f(x) = \begin{cases}1 & x > 0 \\ 0 & x \leq 0\end{cases}`
+    :math:`f(z) = \begin{cases}1 & z > 0 \\ 0 & z \leq 0\end{cases}`
 
     """
-    if x > 0.0:
+    if z > 0.0:
         ret = 1.0
     else:
         ret = 0.0
     return ret
 
 
-def lrelu(x):
+def lrelu(z):
     r"""
     Leaky ReLU
 
-    :math:`f(x) = \begin{cases}x & x > 0 \\ 0.01x & x \leq 0\end{cases}`
+    :math:`f(z) = \begin{cases}z & z > 0 \\ 0.01z & z \leq 0\end{cases}`
 
     """
-    if x > 0.0:
-        ret = x
+    if z > 0.0:
+        ret = z
     else:
-        ret = 0.01 * x
+        ret = 0.01 * z
     return ret
 
-def lrelu_d(x):
+def lrelu_d(z):
     r"""
     Derivada de Leaky ReLU
 
-    :math:`f(x) = \begin{cases}1 & x > 0 \\ 0.01 & x \leq 0\end{cases}`
+    :math:`f(z) = \begin{cases}1 & z > 0 \\ 0.01 & z \leq 0\end{cases}`
 
     """
-    if x > 0.0:
+    if z > 0.0:
         ret = 1
     else:
         ret = 0.01
     return ret
 
 
-def softplus(x):
+def softplus(z):
     r"""
     Softplus
 
-    :math:`f(x)=\log{(1+e^x)}`
+    :math:`f(x)=\log{(1+e^z)}`
 
     """
-    return np.log(1.0 + np.exp(x))
+    return np.log(1.0 + np.exp(z))
 
 
-def softplus_d(x):
+def softplus_d(z):
     r"""
     Derivada de Softplus
 
-    :math:`f(x)=sigmoid(x)=\dfrac{1}{1 + e^{-x}}`
+    :math:`f(z)=sigmoid(z)=\dfrac{1}{1 + e^{-z}}`
 
     """
-    return sigmoid(x)
+    return sigmoid(z)
 
 
-def identity(x):
+def identity(z):
     r"""
     Identidad
 
-    :math:`f(x)=x`
+    :math:`f(z)=z`
 
     """
-    return x
+    return z
 
 
-def identity_d(x):
+def identity_d(z):
     r"""
     Derivada de Identidad
 
-    :math:`f(x)=1`
+    :math:`f(z)=1`
 
     """
     return 1
 
 
-def lecunn_sigmoid(x):
+def lecunn_sigmoid(z):
     r"""
     Sigmoidea recomendada por LeCunn
 
     http://yann.lecun.com/exdb/publis/pdf/lecun-89.pdf
 
-    :math:`f(x)=1.7159 tanh(\dfrac{2x}{3})`
+    :math:`f(z)=1.7159 tanh(\dfrac{2z}{3})`
     """
-    return 1.7159 * np.tanh(x * 2.0/3.0)
+    return 1.7159 * np.tanh(z * 2.0/3.0)
 
 
-def lecunn_sigmoid_d(x):
+def lecunn_sigmoid_d(z):
     r"""
     Derivada de Sigmoidea recomendada por LeCunn
 
-    :math:`f(x)=1.14393 (1 - tanh^2(\dfrac{2x}{3}))`
+    :math:`f(z)=1.14393 (1 - tanh^2(\dfrac{2z}{3}))`
 
     """
-    return 1.7159 * (2.0 / 3.0) * (1.0 - np.tanh(x * 2.0/3.0) ** 2)
+    return 1.7159 * (2.0 / 3.0) * (1.0 - np.tanh(z * 2.0/3.0) ** 2)
 
 # TODO definirlas mejor
-# def sin(x):
-#     return np.sin(x)
+# def sin(z):
+#     return np.sin(z)
 #
 #
-# def sin_d(x):
-#     return np.cos(x)
+# def sin_d(z):
+#     return np.cos(z)
 
 
 fun_activation = {'Tanh': tanh, 'Sigmoid': sigmoid, 'ReLU': relu, 'Softplus': softplus,
