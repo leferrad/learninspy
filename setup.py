22cd501ae15f386c9c1ca2d8c6aece7769ddc39c
Leandro Ferrado <ljferrado@gmail.com>
2016-06-26 17:34:53 -0300

    Arreglo setup y config Travis

--- setup.py
+++ setup.py
@@ -5,20 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="learninspy",
-
     version="0.1.0",
-
     author="Leandro Ferrado",
     author_email="ljferrado@gmail.com",
     url="https://github.com/leferrad/learninspy",
-
-    packages=find_packages(exclude=['examples', 'docs']),
-    scripts=[],
-
+    packages=find_packages(exclude=['examples', 'docs', 'test']),
     license="LICENSE",
-
     description="Framework of deep learning for PySpark",
     long_description=open("README.md").read(),
-
     install_requires=open("requirements.txt").read().split()
 )
