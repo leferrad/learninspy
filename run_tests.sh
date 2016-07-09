d15ac0ac48dec6914051f8e532beec03bb143dd5
Leandro Ferrado <ljferrado@gmail.com>
2016-07-09 00:29:34 -0300

    Arreglando config de Travis

--- run_tests.sh
+++ run_tests.sh
@@ -5,7 +5,7 @@ if [ -z "$SPARK_HOME" ]; then
     exit 1
 fi
 
-export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python/
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:../
 export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python/lib/py4j-0.9-src.zip
 
 export PYTHONWARNINGS="ignore"
