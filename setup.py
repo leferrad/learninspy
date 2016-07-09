39e7f05da4ccd552b3637f668a8e5d69f65eb8c3
Leandro Ferrado <ljferrado@gmail.com>
2016-07-09 17:47:49 -0300

    Actualizo setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name="learninspy",
-
     version="0.1.0",
-
     author="Leandro Ferrado",
     author_email="ljferrado@gmail.com",
     url="https://github.com/leferrad/learninspy",
-
-    packages=find_packages(exclude=['examples', 'docs']),
-    scripts=[],
-
+    packages=find_packages(exclude=['examples', 'docs', 'test']),
     license="LICENSE",
-
     description="Framework of deep learning for PySpark",
     long_description=open("README.md").read(),
-
     install_requires=open("requirements.txt").read().split()
 )
