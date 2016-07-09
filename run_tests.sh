a130c49819e28c006fd1dae596d124ebc568de9d
Leandro Ferrado <ljferrado@gmail.com>
2016-07-08 21:49:51 -0300

    Arreglando config de Travis

--- run_tests.sh
+++ run_tests.sh
@@ -5,11 +5,9 @@ if [ -z "$SPARK_HOME" ]; then
     exit 1
 fi
 
-export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:../
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python/
 export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python/lib/py4j-0.9-src.zip
 
 export PYTHONWARNINGS="ignore"
 
-# nosetests $@ --verbosity 2
-python test/test_metrics.py
-python test/test_autoencoder.py
+nosetests $@ --verbosity 2
