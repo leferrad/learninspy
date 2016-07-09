61c2995e05404a8d6ef5b4cc965c91430620bba2
Leandro Ferrado <ljferrado@gmail.com>
2016-07-09 17:44:25 -0300

    Actualizo config de Travis

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ -z "$SPARK_HOME" ]; then
+    echo 'You need to set $SPARK_HOME to run these tests.' >&2
+    exit 1
+fi
+
+PYFORJ=`ls -1 $SPARK_HOME/python/lib/py4j-*-src.zip | head -1`
+
+export PYTHONPATH=$PYTHONPATH:$SPARK_HOME/python:../
+export PYTHONPATH=$PYTHONPATH:$PYFORJ
+
+export PYTHONWARNINGS="ignore"
+
+nosetests $@ --verbosity 2
