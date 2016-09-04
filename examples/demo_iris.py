33fe8e293026644e6f924a36dc480b60022bbf2a
Leandro Ferrado <ljferrado@gmail.com>
2016-09-03 23:19:15 -0300

    Actualizacion de ejemplos

--- examples/demo_iris.py
+++ examples/demo_iris.py
@@ -1,66 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Ejemplos de uso para entrenar redes neuronales con Learninspy utilizando datos de Iris (clasificación)."""
+
 __author__ = 'leferrad'
 
-# Librerias de Python
-import time
-import os
-
-# Librerias internas
-from learninspy.core import model as mod
+from learninspy.core.model import NeuralNetwork, NetworkParameters
 from learninspy.core.optimization import OptimizerParameters
 from learninspy.core.stops import criterion
-from learninspy.utils.data import StandardScaler, LocalLabeledDataSet
+from learninspy.utils.data import LocalLabeledDataSet, load_iris
 from learninspy.utils.evaluation import ClassificationMetrics
+from learninspy.utils.plots import plot_fitting
+from learninspy.utils.fileio import get_logger
+
+import os
+
+logger = get_logger(name='learninspy-demo_iris')
+
+# Aca conviene hacer de demo:
+# *Examinar diferencias en resultados con diferentes funciones de consenso
+# *Explorar criterios de corte
+# ** MaxIterations de 5 a 10 cambia mucho la duracion final
 
 
-net_params = mod.NetworkParameters(units_layers=[4, 10, 10, 3], activation='Softplus',
-                                   dropout_ratios=[0.5, 0.5, 0.0], classification=True)
+# -- 1) Carga de datos
 
+logger.info("Cargando datos de Iris ...")
+dataset = load_iris()
+dataset = LocalLabeledDataSet(dataset)
+rows, cols = dataset.shape
+logger.info("Dimension de datos: %i x %i", rows, cols)
+
+train, valid, test = dataset.split_data([0.7, 0.1, 0.2])  # Particiono en conjuntos
+
+# -- 2) Selección de parámetros
+
+# --- 2.a) Parámetros de red neuronal
+net_params = NetworkParameters(units_layers=[4, 8, 3], dropout_ratios=[0.0, 0.0],
+                               activation='ReLU', strength_l1=1e-5, strength_l2=3e-4,
+                               classification=True, seed=123)
+
+# --- 2.b) Parámetros de optimización
 local_stops = [criterion['MaxIterations'](10),
                criterion['AchieveTolerance'](0.95, key='hits')]
 
 global_stops = [criterion['MaxIterations'](30),
                 criterion['AchieveTolerance'](0.95, key='hits')]
 
-opt_params = OptimizerParameters(algorithm='Adadelta', stops=local_stops, merge_criter='w_avg')
+options = {'step-rate': 1.0, 'decay': 0.99, 'momentum': 0.7, 'offset': 1e-8}
+
+optimizer_params = OptimizerParameters(algorithm='Adadelta', stops=local_stops, options=options,
+                                       merge_criter='w_avg', merge_goal='hits')
+
+#options = {'step-rate': 0.01, 'momentum': 0.8, 'momentum_type': 'nesterov'}
+
+#opt_params = OptimizerParameters(algorithm='GD', stops=local_stops, options=options)
 
 
-neural_net = mod.NeuralNetwork(net_params)
+logger.info("Optimizacion utilizada: %s", os.linesep+str(optimizer_params))
+logger.info("Configuracion usada: %s", os.linesep+str(net_params))
 
-print "Cargando base de datos ..."
-dataset = LocalLabeledDataSet()
-dataset.load_file(os.path.dirname(os.path.realpath(__file__))+'/datasets/iris.csv')
-print "Size de la data: "
-print dataset.shape
+# -- 3) Construcción y ajuste de red neuronal
 
-print "Creando conjuntos de train, valid y test ..."
-train, valid, test = dataset.split_data([.5, .3, .2])  # Particiono conjuntos
-# Standarize data
-std = StandardScaler()
-std.fit(train)
-train = std.transform(train)
-valid = std.transform(valid)
-test = std.transform(test)
-# Collect de RDD en list
-train = train.collect()
-valid = valid.collect()
-test = test.collect()
+neural_net = NeuralNetwork(net_params)
 
-print "Entrenando red neuronal ..."
-t1 = time.time()
-hits_valid = neural_net.fit(train, valid, valid_iters=1, mini_batch=30, parallelism=4, stops=global_stops,
-                            optimizer_params=opt_params, keep_best=True, reproducible=True)
+logger.info("Entrenando red neuronal ...")
+hits_valid = neural_net.fit(train, valid, valid_iters=1, mini_batch=10, parallelism=2, stops=global_stops,
+                            optimizer_params=optimizer_params, keep_best=True, reproducible=True)
 hits_test, predict = neural_net.evaluate(test, predictions=True)
-t1f = time.time() - t1
 
-print 'Tiempo: ', t1f, 'Tasa de acierto final: ', hits_test
+logger.info("Hits en test: %12.11f", hits_test)
 
-print "Metricas: "
-labels = map(lambda lp: float(lp.label), test)
+# --4) Evaluacion de desempeño
+
+logger.info("Metricas de evaluación en clasificacion: ")
+labels = map(lambda lp: float(lp.label), test.collect())
 metrics = ClassificationMetrics(zip(predict, labels), 3)
-print "Precision: ", metrics.precision()
-print "Recall: ", metrics.recall()
-print "Confusion: "
-print metrics.confusion_matrix()
+logger.info("F1-Score: %12.11f", metrics.f_measure(beta=1))
+logger.info("Precision: %12.11f", metrics.precision())
+logger.info("Recall: %12.11f", metrics.recall())
+logger.info("Matriz de confusion: %s", os.linesep+str(metrics.confusion_matrix()))
+
+# --5) Ploteos
+
+plot_fitting(neural_net)
 
 
 
---
+++ examples/demo_plants.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Ejemplos de uso para entrenar redes neuronales con Learninspy
+utilizando datos de Combined Cycle Power Plant (regresión)."""
+
+__author__ = 'leferrad'
+
+from learninspy.core.model import NeuralNetwork, NetworkParameters
+from learninspy.core.optimization import OptimizerParameters
+from learninspy.core.stops import criterion
+from learninspy.utils.data import LocalLabeledDataSet, StandardScaler, load_ccpp
+from learninspy.utils.evaluation import RegressionMetrics
+from learninspy.utils.plots import plot_fitting
+from learninspy.utils.fileio import get_logger
+
+import os
+
+logger = get_logger(name='learninspy-demo_ccpp')
+
+# Aca conviene hacer de demo:
+# *Examinar diferencias en resultados con diferentes funciones de consenso
+# *Explorar criterios de corte
+# ** MaxIterations de 5 a 20 cambia mucho el resultado final (mejora)
+
+
+# -- 1.a) Carga de datos
+
+logger.info("Cargando datos de Combined Cycle Power Plant ...")
+dataset = load_ccpp()
+dataset = LocalLabeledDataSet(dataset)
+rows, cols = dataset.shape
+logger.info("Dimension de datos: %i x %i", rows, cols)
+
+train, valid, test = dataset.split_data([0.5, 0.3, 0.2])  # Particiono en conjuntos
+
+# -- 1.b) Normalización
+"""
+std = StandardScaler()
+std.fit(train)
+train = std.transform(train)
+valid = std.transform(valid)
+test = std.transform(test)
+"""
+# -- 2) Selección de parámetros
+
+# --- 2.a) Parámetros de red neuronal
+net_params = NetworkParameters(units_layers=[4, 30, 1], dropout_ratios=[0.0, 0.0],
+                               activation='ReLU', strength_l1=5e-7, strength_l2=3e-4,
+                               classification=False, seed=123)
+
+# --- 2.b) Parámetros de optimización
+local_stops = [criterion['MaxIterations'](30),
+               criterion['AchieveTolerance'](0.95, key='hits')]
+
+global_stops = [criterion['MaxIterations'](50),
+                criterion['AchieveTolerance'](0.95, key='hits')]
+
+options = {'step-rate': 1.0, 'decay': 0.995, 'momentum': 0.3, 'offset': 1e-8}
+
+optimizer_params = OptimizerParameters(algorithm='Adadelta', stops=local_stops, options=options,
+                                       merge_criter='avg', merge_goal='hits')
+
+logger.info("Optimizacion utilizada: %s", os.linesep+str(optimizer_params))
+logger.info("Configuracion usada: %s", os.linesep+str(net_params))
+
+# -- 3) Construcción y ajuste de red neuronal
+
+neural_net = NeuralNetwork(net_params)
+
+logger.info("Entrenando red neuronal ...")
+hits_valid = neural_net.fit(train, valid, valid_iters=5, mini_batch=50, parallelism=4,
+                            stops=global_stops, optimizer_params=optimizer_params, measure='R2',
+                            keep_best=True, reproducible=False)
+hits_test, predict = neural_net.evaluate(test, predictions=True)
+
+logger.info("Hits en test: %12.11f", hits_test)
+
+# --4) Evaluacion de desempeño
+
+logger.info("Metricas de evaluación en clasificacion: ")
+labels = map(lambda lp: float(lp.label), test.collect())
+metrics = RegressionMetrics(zip(predict, labels))
+print "MSE: ", metrics.mse()
+print "RMSE: ", metrics.rmse()
+print "MAE: ", metrics.mae()
+print "R-cuadrado: ", metrics.r2()
+print "Explained Variance: ", metrics.explained_variance()
+print zip(predict, labels)[:10]
+
+# --5) Ploteos
+
+plot_fitting(neural_net)
