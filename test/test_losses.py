7606c01e34088b786324a7e9c06d20a46cc17331
Leandro Ferrado <ljferrado@gmail.com>
2016-12-08 11:02:47 -0300

    Actualizo tests

--- test/test_losses.py
+++ test/test_losses.py
@@ -12,7 +12,7 @@ logger = get_logger(name=__name__)
 def test_losses_gradients():
     logger.info("Testeando gradientes implementados para las funciones de error...")
     for loss in fun_loss.keys():  # Dado que hay una regla de cadena de por medio, voy a tener que saltear este check
-        if loss == 'CrossEntropy':  # TODO: idear como hacer check en esta función integrando deriv por regla de cadena
+        if loss == 'CrossEntropy':  # TODO: ver si conviene el check en esta función mediante deriv por regla de cadena
             continue
         logger.info("Gradiente de función %s:", loss)
         check = CheckGradientLoss(loss)
